347a8ecbde7e9ed170deaf5268ebc17552ffa467
Eloi Matos <eloi_matos@hotmail.com>
2021-10-11 10:28:56 -0300

    add AppStore test

--- test/app/app_store_test.dart
+++ test/app/app_store_test.dart
@@ -1,6 +1,8 @@
+import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
+
 import 'package:themes_app/app/app_store.dart';
- 
+
 void main() {
   late AppStore store;
 
@@ -8,9 +10,9 @@ void main() {
     store = AppStore();
   });
 
-  test('increment count', () async {
-    expect(store.value, equals(0));
-    store.increment();
-    expect(store.value, equals(1));
+  test('should change app theme', () async {
+    expect(store.primary, equals(Colors.blue));
+    store.changeTheme(const Color(0xFF34EB8F));
+    expect(store.primary.shade500, equals(const Color(0xFF34EB8F)));
   });
 }
